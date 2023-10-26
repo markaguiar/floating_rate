4c07d7023ae76cb40f7e71e24384eabca1156866
markaguiar <mark@markaguiar.com>
2023-10-26 17:45:31 +0100

    fixed y paths
    Co-authored-by: Manuel Amador <github@amador.email>

--- scripts/IMF_plots.jl
+++ scripts/IMF_plots.jl
@@ -13,9 +13,8 @@ using DataFrames
 
 #get paths of consumption and income 
 function get_cons_path(paths::Vector, shocks_vec::Vector, m)
-    simulation!(paths, shocks_vec, m; n = big_T, trim = 1000, trim_def = 20)
     cons_path=map((j)->paths[j].c, collect(1:big_N))
-    inc_path=map((j)->paths[j].y, collect(1:big_N))
+    inc_path=map((j)->get_y_grid(m)[paths[j].y_ind], collect(1:big_N))
     return cons_path, inc_path
 end;
 
@@ -30,40 +29,86 @@ function u(x,gamma)
 end
 
 
-
 function compute_value(cons, inc, beta, gamma)
-    out= map(x->u(x,gamma),cons)
-    val_zero=0.0
-    for path in out
-        val=0.0
-        for (t,flow) in enumerate(path)
-            val += beta^(t-1)*flow
+
+    val_c_zero = 0.0
+    val_y_zero = 0.0
+
+    for (path_c, path_y) in zip(cons, inc) 
+        val_c = 0.0
+        val_y = 0.0 
+        for (t, (c, y)) in enumerate(zip(path_c, path_y))
+            val_c += beta^(t-1) * u(c, gamma)
+            val_y += beta^(t-1) * u(y, gamma)
         end 
-        val_zero+=val
-    end
+        val_c_zero += val_c 
+        val_y_zero += val_y 
+    end 
 
-    out= map(x->u(x,gamma),inc)
-    val_aut=0.0
-    for path in out
-        val=0.0
-        for (t,flow) in enumerate(path)
-            val += beta^(t-1)*flow
-        end 
-        val_aut+=val
-    end
-
-    val_zero=val_zero/length(cons)
-    val_aut=val_aut/length(inc)
-
+    val_zero = val_c_zero/length(cons)
+    val_aut = val_y_zero/length(inc)
 
     return (val_zero/val_aut)^(1/(1-gamma))
 end
+
+
+# function compute_value(cons, inc, beta, gamma)
+#     out= map(x->u(x,gamma),cons)
+#     val_zero=0.0
+#     for path in out
+#         val=0.0
+#         for (t,flow) in enumerate(path)
+#             val += beta^(t-1)*flow
+#         end 
+#         val_zero+=val
+#     end
+
+#     out2= map(x->u(x,gamma),inc)
+#     val_aut=0.0
+#     for path in out2
+#         val=0.0
+#         for (t,flow) in enumerate(path)
+#             val += beta^(t-1)*flow
+#         end 
+#         val_aut+=val
+#     end
+
+#     val_zero=val_zero/length(cons)
+#     val_aut=val_aut/length(inc)
+
+
+#     return (val_zero/val_aut)^(1/(1-gamma))
+# end
    
+
+
+function do_beta_simulations!(beta_df, models, benchmark_parameters, shocks, paths, betaGrid)
+    for j in eachindex(models)
+        m=models[j]
+        gamma0=benchmark_parameters.γ
+        
+        simulation!(paths, shocks, m; n = big_T, trim = 1000, trim_def = 20)
+        cons_paths, inc_paths=get_cons_path(paths,shocks,m);
+        cons_equiv = ThreadsX.map(beta->compute_value(cons_paths,inc_paths,beta,gamma0), betaGrid)
+        insertcols!(beta_df, j=>cons_equiv)
+    end
+end 
+
+function do_gamma_simulations!(gamma_df, models, benchmark_parameters, shocks, paths, gammaGrid)
+    for j in eachindex(models)
+        m=models[j]
+        beta0= benchmark_parameters.pref.β #1/benchmark_parameters.R 
+        cons_paths, inc_paths=get_cons_path(paths,shocks,m);
+        cons_equiv= ThreadsX.map(gamma->compute_value(cons_paths,inc_paths,beta0,gamma), gammaGrid)
+        insertcols!(gamma_df, j=>cons_equiv)
+    end
+end 
+
 
 benchmark_parameters =  let
     R = 1.01
     β = 0.9540232420
-    γ=2
+    γ = 2
     pref = Preferences(β = β, u = make_CRRA(ra = γ))
     y = discretize(YProcess(n = 50, ρ = 0.948503, std = 0.027092, μ = 0.0, span = 3.0, tails = false))
     m = MTruncatedNormal(; std = 0.003, span = 2.0, quadN = 100)
@@ -72,6 +117,9 @@ benchmark_parameters =  let
 
     (R = R, pref = pref, y = y, m = m, penalty = penalty, η = η,γ=γ)
 end;
+
+
+
 
 models = let
     R, pref, y, m, penalty, η = benchmark_parameters
@@ -118,7 +166,7 @@ models = let
     )
 
 
-    (; egLT, egST, ckST) 
+   (; egLT, egST, ckST) 
 end;
 
 # compute models
@@ -126,6 +174,7 @@ end;
     @time solve!(m; print_every = 200, max_iters = 5_000, err = 1e-7)
 end
 
+println("Simulations")
 
 #simulations
 big_T = 20_000 
@@ -134,32 +183,14 @@ big_N = 1_000
 #generate shock path 
 rng = Random.seed!(1234)
 shocks, paths = create_shocks_paths(models.ckST, big_T, big_N; rng) # make sure to use a ck model to draw the sunspots
+
 betaGrid=LinRange(0.95,0.999,50)
 beta_df=DataFrame("Beta"=>betaGrid)
-
-println("Simulations")
-
-for j in eachindex(models)
-    m=models[j]
-    beta0=benchmark_parameters.pref.β
-    gamma0=benchmark_parameters.γ
-    cons_paths, inc_paths=get_cons_path(paths,shocks,m);
-    cons_equiv= ThreadsX.map(beta->compute_value(cons_paths,inc_paths,beta,gamma0), betaGrid)
-    insertcols!(beta_df, j=>cons_equiv)
-end
-
-gammaGrid=LinRange(0.5,10,50)
+gammaGrid=LinRange(0.5,20,50)
 gamma_df=DataFrame("Gamma"=>gammaGrid)
 
-for j in eachindex(models)
-    m=models[j]
-    beta0= 0.98 #benchmark_parameters.pref.β #1/benchmark_parameters.R 
-    gamma0=benchmark_parameters.γ
-    cons_paths, inc_paths=get_cons_path(paths,shocks,m);
-    cons_equiv= ThreadsX.map(gamma->compute_value(cons_paths,inc_paths,beta0,gamma), gammaGrid)
-    insertcols!(gamma_df, j=>cons_equiv)
-end
-
+@time do_beta_simulations!(beta_df, models, benchmark_parameters, shocks, paths, betaGrid)
+@time do_gamma_simulations!(gamma_df, models, benchmark_parameters, shocks, paths, gammaGrid)
 
 
 # Some plots
@@ -183,11 +214,26 @@ vline!([rstar,rho0],color=:LightGray,lw=3)
 plot(gamma_df.Gamma,100*(gamma_df.egLT.-1),color=:black,lw=3,legend=false)
 plot!(gamma_df.Gamma,100*(gamma_df.egST.-1),ls=:dash,color=:black,lw=3)
 plot!(gamma_df.Gamma,100*(gamma_df.ckST.-1),ls=:dashdot,color=:green,lw=3)
-xlims!(0.4,10)
+xlims!(0.4,20)
 ylims!(-2,2)
 yticks!([-2, -1, 0, 1, 2],["-2%","-1%","0","1%","2%"])
 xlabel!("Household Risk Aversion: " * L"\gamma")
 hline!([0],color=:black, lw=1)
 vline!([benchmark_parameters.γ],color=:LightGray,lw=3)
 
+
+computed_moments = 
+    map(
+        (; models.egST, models.ckST, models.egLT)) do (m) 
+        simulation!(paths, shocks, m; n = big_T, trim = 1000, trim_def = 20)
+        out = moments(paths, m)
+        (; out.mean_bp_y, out.mean_mv_y, out.mean_spread, out.std_spread, out.max_spread,  out.std_c_y, out.cor_tb_y, out.cor_r_y, out.cor_r_b_y, out.cor_r_tb, out.def_rate, out.run_share)
+    end
+
+pretty_table(
+    collect(map(m -> pairs(m), computed_moments)),
+    row_names = collect(keys(computed_moments)), 
+#    backend = Val(:html),
+    formatters = ( ft_printf("%5.2f"), ft_printf("%5.3f",[3,4]))
+)
 
